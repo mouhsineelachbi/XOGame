944c11dd10ac4c599ff6e5bf343eeff2f82958df
mouhsineelachbi <mouhsineelachbi@gmail.com>
2020-12-31 00:05:15 +0100

    square value as a button

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      <button>{{ value }}</button>
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
+
+  constructor () {
+    this.value = null ;
+  }
+
+}
